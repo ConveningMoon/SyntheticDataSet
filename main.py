807fb5ab309c095e3ba0eaadef323ec7f0beb7c1
ConveningMoon <dj.vergara@hotmail.com>
2024-04-23 08:00:23 +0500

    Use new model

--- main.py
+++ main.py
@@ -4,52 +4,50 @@ from diffusers import DiffusionPipeline
 import torch
 import random
 import os
-import matplotlib.pyplot as plt
 
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipeline = DiffusionPipeline.from_pretrained("SG161222/RealVisXL_V4.0", torch_dtype=torch.float16)
 pipeline.to("cuda")
 
 ### generate images ###
-# Creating directories for new emotions
-os.makedirs('./content/faces/happy', exist_ok=True)
-os.makedirs('./content/faces/sad', exist_ok=True)
-os.makedirs('./content/faces/angry', exist_ok=True)
-os.makedirs('./content/faces/surprised', exist_ok=True)
-os.makedirs('./content/faces/bored', exist_ok=True)  # For bored faces
-os.makedirs('./content/faces/tired', exist_ok=True)  # For tired faces
-os.makedirs('./content/faces/neutral', exist_ok=True)  # For neutral faces
-os.makedirs('./content/faces/disgust', exist_ok=True)  # For faces showing disgust
+# Ensuring directories for all emotions exist
+emotions = ['smiling', 'surprised']
+for emotion in emotions:
+    os.makedirs(f'./content/faces/{emotion}', exist_ok=True)
 
-ethnicities = ['a latino', 'a white', 'a black', 'a middle eastern', 'an indian', 'an asian']
+# Expanding the ethnicities to cover a more global representation
+ethnicities = ['a latino', 'a white', 'a black', 'a middle eastern', 'an indian', 'an asian', 'an african',
+               'an eastern european', 'a southeast asian', 'a pacific islander']
 genders = ['male', 'female']
 
-# Updated emotion_prompts dictionary with added emotions
-emotion_prompts = {'happy': 'smiling',
-                   'sad': 'frowning, sad face expression, crying, emotional, depressed',
-                   'surprised': 'surprised, opened mouth, raised eyebrows',
-                   'angry': 'angry',
-                   'bored': 'bored, lack of interest, dull expression, downturned mouth and half-eyed gaze',
-                   'tired': 'tired, yawning, droopy eyes',
-                   'neutral': 'neutral expression, straight face, looking directly at the camera',
-                   'disgust': 'disgusted, scrunched nose, The lips curling into a snarling frown, the nostrils '
-                              'flaring, the cheeks pushing up to squinting lower lids and a furrowed brow'}
+# Correcting the emotion_prompt dictionary to ensure clear representation of emotions
+emotion_prompts = {
+    'smiling': 'slight smile with no visible teeth, soft eyes, closed mouth, no teeth, no open mouth, mouth closed, none teeth',
+    'surprised': 'surprised, mouth wide open, raised eyebrows, eyes wide open, natural expression, perfect expression, natural face'
+}
+
+# Define the age range for the generated images
+age_range = "15-35 years old"
+
 counter = 0
-for j in range(2000):
+for j in range(5):
     counter += 1
     for emotion, emotion_prompt in emotion_prompts.items():
         ethnicity = random.choice(ethnicities)
         gender = random.choice(genders)
 
-        prompt = f'Medium-shot portrait of {ethnicity} {gender}, {emotion_prompt}, front view, looking at the camera, ' \
-                 f'color photography, ' + \
-                 'photorealistic, hyperrealistic, realistic, incredibly detailed, crisp focus, digital art, ' \
-                 'depth of field, 50mm, 8k'
-        negative_prompt = '3d, cartoon, anime, sketches, (worst quality:2), (low quality:2), (normal quality:2), ' \
-                          'lowres, normal quality, ((monochrome)), ' + \
-                          '((grayscale)) Low Quality, Worst Quality, plastic, fake, disfigured, deformed, blurry, ' \
-                          'bad anatomy, blurred, watermark, grainy, signature'
+        # Include the age range condition in the prompt
+        prompt = f'Medium-shot portrait of {ethnicity} {gender}, age {age_range}, {emotion_prompt}, front view, ' \
+                 'looking at the camera, color photography, photorealistic, hyperrealistic, ' \
+                 'incredibly detailed, crisp focus, digital art, depth of field, 50mm'
 
+        negative_prompt = '3d, cartoon, anime, sketches, worst quality:2, low quality:2, ' \
+                          'normal quality:2, low resolution, lowres, monochrome, grayscale, plastic, fake, ' \
+                          'disfigured, mutant,  defective anatomy, deformed, blurry, bad anatomy, blurred, watermark, ' \
+                          'grainy, signature, elderly, infant, child, old, young child, deformed, two mouths, mutant, robot, strange, monster'
+
+        # Generate the image
         img = pipeline(prompt, negative_prompt=negative_prompt).images[0]
-        img.save(f'./content/faces/{emotion}/{str(j).zfill(4)}.png')
+        img_path = f'./content/faces/{emotion}/{str(j).zfill(5)}.png'
+        img.save(img_path)
 
-    print(counter)
+    print(f'Generated {counter} sets of images')
--- test.py
+++ test.py
@@ -4,42 +4,54 @@ from diffusers import DiffusionPipeline
 import torch
 import random
 import os
-import matplotlib.pyplot as plt
 
 pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
 pipeline.to("cuda")
 
 ### generate images ###
-# Creating directories for new emotions
-os.makedirs('./content/faces/fear', exist_ok=True)  # For faces showing disgust
-os.makedirs('./content/faces/confused', exist_ok=True)  # For faces showing disgust
+# Ensuring directories for all emotions exist
+emotions = ['happy', 'smiling', 'neutral', 'surprised', 'angry', 'sad']
+for emotion in emotions:
+    os.makedirs(f'./content/faces/{emotion}', exist_ok=True)
 
-ethnicities = ['a Latino', 'a White', 'a Black', 'a Middle Eastern', 'an Indian', 'an Asian', 'a Southeast Asian', 'a North African']
+# Expanding the ethnicities to cover a more global representation
+ethnicities = ['a latino', 'a white', 'a black', 'a middle eastern', 'an indian', 'an asian', 'an african',
+               'an eastern european', 'a southeast asian', 'a pacific islander']
 genders = ['male', 'female']
 
-# Updated emotion_prompts dictionary with added emotions
+# Correcting the emotion_prompt dictionary to ensure clear representation of emotions
 emotion_prompts = {
-    'fear': 'face showing fear: wide open eyes, eyebrows raised and curved, mouth open slightly, facial muscles tense, subtle trembling, cold sweat visible',
-    'confused': 'face showing confusion: eyebrows furrowed and drawn together, slight frown, eyes looking off to the side, mouth parted as if mid-question'
+    'happy': 'smiling with joy, a happy face expression, eyes crinkling, wide beaming smile, visible teeth',
+    'smiling': 'content, slight smile with no visible teeth, soft eyes, closed mouth',
+    'neutral': 'neutral, straight face, relaxed features, looking directly at the camera',
+    'surprised': 'surprised, mouth wide open, raised eyebrows, eyes wide open',
+    'angry': 'angry, furious, frowning, narrowed eyes, tight lips, flared nostrils',
+    'sad': 'sad face expression, crying, emotional, depressed, intense sadness'
 }
 
+# Define the age range for the generated images
+age_range = "15-35 years old"
+
 counter = 0
-for j in range(400):
+for j in range(10):
     counter += 1
     for emotion, emotion_prompt in emotion_prompts.items():
         ethnicity = random.choice(ethnicities)
         gender = random.choice(genders)
 
-        prompt = f'Medium-shot portrait of {ethnicity} {gender}, {emotion_prompt}, front view, looking at the camera, ' \
-                 f'color photography, ' + \
-                 'photorealistic, hyperrealistic, realistic, incredibly detailed, crisp focus, digital art, ' \
-                 'depth of field, 50mm, 8k'
-        negative_prompt = '3d, cartoon, anime, sketches, (worst quality:2), (low quality:2), (normal quality:2), ' \
-                          'lowres, normal quality, ((monochrome)), ' + \
-                          '((grayscale)) Low Quality, Worst Quality, plastic, fake, disfigured, deformed, blurry, ' \
-                          'bad anatomy, blurred, watermark, grainy, signature'
+        # Include the age range condition in the prompt
+        prompt = f'Medium-shot portrait of {ethnicity} {gender}, age {age_range}, {emotion_prompt}, front view, ' \
+                 'looking at the camera, color photography, photorealistic, hyperrealistic, ' \
+                 'incredibly detailed, crisp focus, digital art, depth of field, 50mm, 8k'
 
+        negative_prompt = '3d, cartoon, anime, sketches, worst quality:2, low quality:2, ' \
+                          'normal quality:2, lowres, monochrome, grayscale, plastic, fake, ' \
+                          'disfigured, deformed, blurry, bad anatomy, blurred, watermark, ' \
+                          'grainy, signature, elderly, infant, child, old, young child'
+
+        # Generate the image
         img = pipeline(prompt, negative_prompt=negative_prompt).images[0]
-        img.save(f'./content/faces/{emotion}/{str(j).zfill(4)}.png')
+        img_path = f'./content/faces/{emotion}/{str(j).zfill(5)}.png'
+        img.save(img_path)
 
-    print(counter)
+    print(f'Generated {counter} sets of images')
